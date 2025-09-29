e1fed424ff5e57297482672b544db5483d79da27
Yorky3321 <magicyork666@gmail.com>
2025-09-29 21:02:05 +0800

    建立第一個路由

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from BeautifulSoup import BeautifulSoup
+import requests
+import Selenium
 
 # Create your views here.
+def crawl(request):
+    url = "http://example.com"  # 替換為你想爬取的網站
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # 假設我們想抓取所有的標題
+    titles = soup.find_all('h1')
+    title_list = [title.get_text() for title in titles]
+    
+    return render(request, 'crawler/your_template.html', {'titles': title_list})
